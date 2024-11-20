59f949772365199f81cc70ffd16bc70135eeb952
Chris Fidao <fideloper@gmail.com>
2024-11-20 09:09:29 -0500

    some testing around stdout/stderr output in jobs / scheduler

---
+++ app/Console/Commands/TestSchedulerOutput.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class TestSchedulerOutput extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-scheduler-output';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Log::info("Log::info(): testing scheduler output");
+        $this->info('$this->info(): testing scheduler output');
+        $this->error('$this->error(): testing scheduler output');
+        $this->output->getErrorStyle()->writeln('to stderr');
+    }
+}
