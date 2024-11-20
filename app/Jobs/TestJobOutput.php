c7b4dfecf75d8a9cba6cc4ada64bbee8954500db
Chris Fidao <fideloper@gmail.com>
2024-11-20 09:09:29 -0500

    some testing around stdout/stderr output in jobs / scheduler

---
+++ app/Jobs/TestJobOutput.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
+
+class TestJobOutput implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Log::info("Log::info(): testing job output");
+    }
+}
