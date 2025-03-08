492c9ed059642d7ac174a7f5c8e117a0ce77800a
Chris Fidao <fideloper@gmail.com>
2025-03-08 11:09:03 -0500

    test out some x-frames thing

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,28 @@
 <?php
 
+use App\Console\Commands\TestSchedulerOutput;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     \Illuminate\Support\Facades\Log::info("okay from web request now");
     return view('welcome');
+});
+
+Route::get('/text-x-frame-default', function() {
+    return response('default value', 200);
+});
+
+Route::get('/text-x-frame-over-ride', function() {
+    return response('value: SAMEORIGIN', 200)->withHeaders([
+        'X-Frame-Options' => 'SAMEORIGIN',
+    ]);
+});
+
+Route::get('/text-x-frame-unset', function() {
+    return response('value: UNSET', 200)->withHeaders([
+        'X-Frame-Options' => 'UNSET',
+    ]);
 });
 
 Route::get('/dashboard', function () {
